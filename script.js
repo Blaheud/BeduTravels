10374518d2a48fb56c4702d5f547524838300961
Blaheud <jannaytapia@gmail.com>
2020-04-25 15:21:43 -0500

    adds index.html and script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+// var myBox;
+// // strings
+// "string"
+// 'string'
+// `string`
+
+// // boolean
+// true
+// false
+
+// // Numbers
+// 123 
+// 1.23
+// 123123123123
+
+// {
+//  color: "green",
+//  age: "12" 
+// }
+
+// //Function
+// function myFunction() {
+//   return 223;
+// }
+
+// //Arrow function
+// () => {
+//   return 223;
+// }
+var lookout = "Jannay"
+
+console.log('Welcome to BEDU Travels ' + lookout + "!")
+
+// document.querySelector('#search-button').addEventListener("click", ()=> {
+//   var lookout = document.querySelector("#search-input").value;
+//   alert("User wants to go to " + lookout);
+// });
+
+// $('#search-button').on('click', ()=> {
+//   var lookout = $('#search-input').val();
+//   alert("User wants to go to " + lookout);
+// });
